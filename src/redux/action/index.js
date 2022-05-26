c1b7d67e1fc3b7744f43fb03fa38838a2019f1a2
Daniel Tostes <ohtosttnz@gmail.com>
2022-05-26 20:12:14 -0300

    feat: Cria action para resetar o placar

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,6 +1,7 @@
 export const LOGIN = 'LOGIN';
 export const ADD_SCORE = 'ADD_SCORE';
 export const SAVE_HASH = 'SAVE_HASH';
+export const RESET_SCORE = 'RESET_SCORE';
 
 export const login = (name, gravatarEmail) => ({
   type: LOGIN,
@@ -16,4 +17,8 @@ export const ACTION_ADD_SCORE = (score) => ({
 export const ACTION_SAVE_HASH = (gravatarHash) => ({
   type: SAVE_HASH,
   gravatarHash,
+});
+
+export const ACTION_RESET_SCORE = () => ({
+  type: RESET_SCORE,
 });
