7c9750c4a4a584d74ce57e72f1fe523245ef8269
Leonardo Lima <leoblima00@gmail.com>
2022-05-26 17:58:21 -0300

    Envia alterações de indentação

--- src/tests/helpers/21.GamePage.test.js
+++ src/tests/helpers/21.GamePage.test.js
@@ -4,13 +4,17 @@ import renderWithRouterAndRedux from './renderWithRouterAndRedux';
 import userEvent from '@testing-library/user-event';
 import Game from '../../pages/Game';
 
-describe('Testa a página de Game requisito do Requisito 5', () => {     const { history } = renderWithRouterAndRedux(<Game />);
+describe('Testa a página de Game requisito do Requisito 5', () => {  
+  test('Se o header é renderizado corretamente', () => { 
+    const { history } = renderWithRouterAndRedux(<Game />);
 
-// Esperamos que seja renderizado na rota '/'
-expect(history.location.pathname).toBe('/');
-
-// Vamos pegar os elementos que esperamos encontrar na página 
-const nameInput = screen.getByTestId('input-player-name');
-const emailInput = screen.getByTestId('input-gravatar-email');
-expect(nameInput).toBeInTheDocument();
-expect(emailInput).toBeInTheDocument(); })
+  // Esperamos que seja renderizado na rota '/'
+    expect(history.location.pathname).toBe('/');
+ 
+ // Vamos pegar os elementos que esperamos encontrar na página 
+    const nameInput = screen.getByTestId('input-player-name');
+    const emailInput = screen.getByTestId('input-gravatar-email');
+    expect(nameInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument(); 
+  })
+})
