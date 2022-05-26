6e9a4d0e96684bcef15a06fb612a2fcd45483cb2
Daniel Tostes <41529552+tostesdaniel@users.noreply.github.com>
2022-05-26 18:55:29 -0300

    Renderiza botão de Logout dentro de Ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Logout from '../components/Logout';
 
 export default class Ranking extends React.Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <Logout />
+      </>
     );
   }
 }
